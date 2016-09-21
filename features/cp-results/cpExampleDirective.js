ab1af39ba584b6526920a8ba3d2e9936152c6fc9
andrewplan <a.plan88@gmail.com>
2016-09-20 22:30:18 -0500

    Finished cutting down redundant directives for chord progression result directives, and examples within song-builder.  Now need to get the examples down to one directive across all views.

--- features/cp-results/cpExampleDirective.js
+++ features/cp-results/cpExampleDirective.js
@@ -1,24 +1,55 @@
 angular.module( 'perfectChordsApp' )
     .directive( 'cpExample', function() {
         return {
-            templateUrl: './features/cp-results/cp-example-directive-tmpl.html'
+            templateUrl: function( elem, attr ) {
+              if ( attr.type === 'result-example' ) {
+                return './features/cp-results/cp-example-directive-tmpl.html'
+              }
+              else if ( attr.type === 'separate-example' ) {
+                return './features/song-builder/song-builder-example-directive-tmpl.html';
+              }
+              else if ( attr.type === 'nested-example' ) {
+                return './features/song-builder/song-builder-prog-example-directive-tmpl.html'
+              }
+            }
             , restrict: 'EA'
-            , require: '^cpResult'
             , replace: true
             , scope: {
                   example: '='
+                  , type: '='
               }
             , controller: 'cpExampleCtrl'
             , link: function( scope, elem, attr ) {
-                  scope.progName = elem.parent().parent().parent().find( 'h3' ).text().replace(' ', '');
+                  // scope.progName = elem.parent().parent().parent().find( 'h3' ).text().replace(' ', '');
 
-                  scope.songInfo = angular.element( elem[ 0 ].querySelector( '.cp-result-example-song-info' ) );
+                  scope.songInfoResultExample = angular.element( elem[ 0 ].querySelector( '.cp-result-example-song-info' ) );
 
-                  scope.songInfo.on( 'click', function() {
+                  scope.songInfoResultExample.on( 'click', function() {
                       elem.parent().parent().find( 'section' ).html(
                           '<iframe src="' + scope.example.url.replace(/http/gi, 'https') + '"' + '></iframe>'
                       );
                   } );
+
+                  scope.songInfoSeparateExample = angular.element( elem[ 0 ].querySelector( '.song-builder-example-song-info') );
+
+                  scope.toggleDropdownBox = function() {
+                    scope.isDropdownBoxActive = !scope.isDropdownBoxActive;
+                  };
+
+                  scope.songInfoSeparateExample.on( 'click', function() {
+                    scope.songInfoSeparateExample.parent().parent().find( 'section' ).html(
+                      '<iframe src="' + scope.example.url + '"' + '></iframe>'
+                    )
+                  } );
+
+                  scope.songInfoNestedExample = angular.element( elem[ 0 ].querySelector( '.song-builder-prog-example-song-info' ) );
+                  scope.progName = elem.parent().parent().parent().find( 'h3' ).text().replace(' ', '');
+
+                  scope.songInfoNestedExample.on( 'click', function() {
+                      elem.parent().parent().find( 'section' ).html(
+                          '<iframe src="' + scope.example.url + '"' + '></iframe>'
+                      )
+                  } );
               }
-        };
-    } );
+          }
+  } );
--- features/song-builder/songBuilderViewCtrl.js
+++ features/song-builder/songBuilderViewCtrl.js
@@ -2,5 +2,4 @@ angular.module( 'perfectChordsApp' )
     .controller( 'songBuilderCtrl', function( $scope, userService, dragulaService ) {
         $scope.progFavorites = userService.getProgFavorites();
         $scope.exampleFavorites = userService.getExampleFavorites();
-
     } );
